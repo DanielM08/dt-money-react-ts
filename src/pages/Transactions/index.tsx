cbc44302d269d7d00771069ee543ca2f12bc3796
daniel.carvalho <daniel.carvalho@maxmilhas.com.br>
2022-10-20 08:42:17 -0300

    Styling transaction page

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,41 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
+import { PriceHightLight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
   return (
     <div>
       <Header />
-
       <Summary />
-    </div>)
+      
+      <TransactionsContainer>
+        <SearchForm />
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHightLight variant="income">
+                  R$ 12.00,00
+                </PriceHightLight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburger</td>
+              <td>
+                <PriceHightLight variant="outcome">
+                  R$ -59,00
+                </PriceHightLight>
+              </td>
+              <td>Alimentação</td>
+              <td>13/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </div>
+  )
 }
